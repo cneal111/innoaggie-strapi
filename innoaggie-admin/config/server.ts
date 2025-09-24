8045639341f9ecd524ee6edf0656fb3eda25b249
Cody Neal <codyrneal1@gmail.com>
2025-09-23 20:41:16 -0500

    updated strapi configs

--- innoaggie-admin/config/server.ts
+++ innoaggie-admin/config/server.ts
@@ -1,7 +1,15 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
+
+  // This is the public-facing URL that Strapi will use when generating links
+  // (reset password emails, admin calls back, etc.)
+  url: env('PUBLIC_URL', 'https://innoaggie.com'),
+
+  // Important when running behind Nginx/Traefik/ELB — makes Strapi honor
+  // X-Forwarded-* headers and not think it's always http://localhost:1337
+  proxy: true,
+
   app: {
     keys: env.array('APP_KEYS'),
   },
